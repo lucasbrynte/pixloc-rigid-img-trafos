b21fca7646543aaa40498235ebae87a04e428dcc
Georg Bökman <bokman@chalmers.se>
2021-12-22 12:09:19 +0100

    Quick bugfix, download script runs again

--- pixloc/pixlib/geometry/utils.py
+++ pixloc/pixlib/geometry/utils.py
@@ -52,11 +52,11 @@ def undistort_points(pts, dist):
         k1, k2 = dist[..., :2].split(1, -1)
         if (not k1.isnan().all()) and k1.isnan().any():
             raise ValueError("Mixture of finite and NaN distortion parameters")
-        if k1.isnan().any and ndist>2:
+        if k1.isnan().any() and ndist>2:
             raise ValueError("Cannot use more than two parameter for NaN distortion parameters")
             
         r2 = torch.sum(pts**2, -1, keepdim=True)
-        if not k1.isnan().any() and not k1.isnan().any: # if in arctan case, no tangential distortion needed:  #
+        if not k1.isnan().any(): # if in arctan case, no tangential distortion needed:  #
             radial = k1*r2 + k2*r2**2
             undist = undist + pts * radial
 
@@ -123,7 +123,7 @@ def J_undistort_points(pts, dist):
             J_diag = f*J_diag + g * norm_pts**2
             J_cross = g*torch.prod(pts, -1, keepdim=True)
 
-        if ndist >2 and not k1.isnan().any: # if in arctan case, no tangential distortion needed
+        if ndist >2 and not k1.isnan().any(): # if in arctan case, no tangential distortion needed
             p12 = dist[..., 2:]
             p21 = p12.flip(-1)
             J_diag += 2*p12*pts.flip(-1) + 6*p21*pts
