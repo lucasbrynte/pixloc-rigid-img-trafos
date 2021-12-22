7dac4644f81633e61a631d1ce43cb459d3201eac
Georg Boekman <georgbokman@outlook.com>
2021-12-22 17:00:50 +0100

    Hack for correct datapath in singularity container

--- pixloc/settings.py
+++ pixloc/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-root = Path(__file__).parent.parent  # top-level directory
+# root = Path(__file__).parent.parent  # top-level directory
+root = Path(".")  # Hack to run on singularity. Always run in pixloc root dir.
 DATA_PATH = root / 'datasets/'  # datasets and pretrained weights
 TRAINING_PATH = root / 'outputs/training/'  # training checkpoints
 LOC_PATH = root / 'outputs/hloc/'  # localization logs
